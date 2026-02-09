017d8ba86516ed8b8eed7997351031eb1ef62a3c
Polarbear <kkamang03@gmail.com>
2026-02-09 17:05:19 +0900

    충돌 해결

--- src/kosta/controller/GoodsController.java
+++ src/kosta/controller/GoodsController.java
@@ -5,6 +5,7 @@ public class GoodsController {
 
     public void test() {
         System.out.println("원격에서 test 추가함 - 시나리오 5번 테스트");
+        System.out.println("로컬에서 수정하고 있어요........");
     }
 
     public void selectAll() {
