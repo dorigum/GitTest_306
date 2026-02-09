3fbec232f097094d254b8224e63a7a4b7b45ac5f
Polarbear🐻‍❄️ <41736468+dorigum@users.noreply.github.com>
2026-02-09 16:57:39 +0900

    Add test output message to GoodsController

--- src/kosta/controller/GoodsController.java
+++ src/kosta/controller/GoodsController.java
@@ -4,7 +4,7 @@ public class GoodsController {
     int i = 10;
 
     public void test() {
-        
+        System.out.println("원격에서 test 추가함 - 시나리오 5번 테스트");
     }
 
     public void selectAll() {
